8bca325b01a49a04661050555cff1d3068033553
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-04-30 13:10:50 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :customers,skip: [:passwords,], controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  scope module: :customer do
+    root to 'homes/top'
+    resources :customers
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
