73b758a9361f08f6c51cb3f0d63c588a5a81635b
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-05-07 07:28:54 +0000

    Change

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,35 @@ Admin.create!(
     email: "sample@sample",
     password: "password"
 )
+
+Genre.create!(
+ [
+  { 
+    id: 1,
+    name: "ケーキ"
+  },
+  { 
+    id: 2,
+    name: "プリン"
+  }
+ ]
+)
+
+Item.create!(
+    genre_id: 1,
+    name: "いちごのショートケーキ(ホール)",
+    caption: "栃木県産のとちおとめを贅沢に使用しています。",
+    price: 2500,
+    is_sold: true
+)
+
+item = Item.create!(
+    genre_id: 2,
+    name: "カスタードプリン",
+    caption: "カスタードを使用したプリンです。",
+    price: 300,
+    is_sold: false
+)
+
+item.image.attach(io: File.open(Rails.root.join('app/assets/images/sample01.jpg')),
+                  filename: 'sample01.jpg')
