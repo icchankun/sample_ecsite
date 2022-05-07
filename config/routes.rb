8bf783625291b9db547db186f31f2c91d8236740
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-05-07 06:46:47 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :customer do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :customer do
     get 'cart_items/index'
   end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -25,6 +29,8 @@ Rails.application.routes.draw do
         delete 'destroy_all'
       end
     end
+    
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
 
   devise_for :customers, skip: [:passwords], controllers: {
