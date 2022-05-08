2f22c1d263cdebb4ba8e8a0fb8af504aef3c93a1
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-05-08 09:16:21 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  namespace :customer do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :customer do
-    get 'cart_items/index'
-  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
@@ -31,6 +24,13 @@ Rails.application.routes.draw do
     end
     
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    
+    resources :orders, only: [:index, :new, :show] do
+      collection do
+        get 'confirm'
+        get 'complete'
+      end
+    end
   end
 
   devise_for :customers, skip: [:passwords], controllers: {
@@ -46,6 +46,10 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     
     resources :genres, only: [:index, :create, :show, :edit, :update]
+    
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
