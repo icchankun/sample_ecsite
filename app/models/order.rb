0a47a0ad97517187bb4933acd2d68647c3f755aa
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-05-09 13:13:04 +0000

    add enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { is_waiting: 0, is_confirming: 1, is_making: 2, is_preparing: 3, completed: 4 }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,3 @@
 class OrderDetail < ApplicationRecord
+  enum making_status: { cannot_make: 0, is_waiting: 1, is_making: 2, completed: 3 }
 end
