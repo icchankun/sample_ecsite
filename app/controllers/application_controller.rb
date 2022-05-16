d06d32f8a3018552d2d7cf364fe452d0f7da75b8
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-05-16 12:44:42 +0000

    Change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,5 +24,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email])
   end
-
 end
